da81dd9cc4f61b16766e21ca6285fbfff1e0cd04
wangzp <6010625@qq.com>
2019-08-15 21:22:00 +0800

    加入 热点

--- pages/index/index.js
+++ pages/index/index.js
@@ -67,12 +67,6 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.request({
-      url: 'data/swiper.json',
-      method: 'GET',
-      success(rsp, status) {
-        console.log(rsp)
-      }
-    });
+    
   }
 })

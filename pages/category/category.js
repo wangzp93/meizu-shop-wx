0f20b9611878b00153481e3f871c20966110988f
wangzp <6010625@qq.com>
2019-09-07 22:46:34 +0800

    全部完成

--- pages/category/category.js
+++ pages/category/category.js
@@ -10,6 +10,15 @@ Page({
       { enName: 'audio', chName: '声学' },
       { enName: 'section', chName: '配件' },
       { enName: 'surround', chName: '周边' }
-    ]
+    ],
+    active: '0'
+  },
+
+  // 改变tab
+  changeTab(e) {
+    let index = e.target.dataset.i;
+    if (index != undefined) {
+      this.setData({ active: index});
+    }
   }
 })
